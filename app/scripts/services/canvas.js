c1081a7f9a701a7831501da5c3f3d44f9514a62e
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-12 20:36:58 +0100

    Add some private properties to canvas factory
    Add general private properties, private setProperties function and
    private getLayers function to canvas factory.

--- app/scripts/services/canvas.js
+++ app/scripts/services/canvas.js
@@ -3,8 +3,39 @@
 angular.module('meanWhiteboardApp')
   .factory('canvasFactory', function () {
     // Service logic
-    // ...
 
-    // Public API here
+    // General properties
+    var properties = {
+	brushWidth: 5,
+	brushCap: 'round',
+	eraserWidth: 5,
+	eraserCap: 'round',
+	pencilWidth: 5,
+	pencilCap: 'round',
+	firstColor: '#000',
+	secondColor: '#fff'
+    };
+
+    var layers = [];
+
+    var setProperties = function(updatedProperties) {
+	    updatedProperties = updatedProperties || {};
+
+	    var prop;
+	    for (prop in updatedProperties) {
+	        if (updatedProperties.hasOwnProperty(prop)) {
+			properties[prop] = updatedProperties.prop;
+		}
+	    }
+    };
+
+    var getLayers = function() {
+	    return layers;
+    };
+
+    var addLayer = function() {
+    
+    };
+
     return {};
   });
