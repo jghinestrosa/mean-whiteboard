3ef5145b91f64768d8147e20f014acce334154bf
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-13 03:01:51 +0100

    Create Layer constructor

--- app/scripts/services/canvas.js
+++ app/scripts/services/canvas.js
@@ -5,38 +5,55 @@ angular.module('meanWhiteboardApp')
 
     // General properties
     var properties = {
-	brushWidth: 5,
-	brushCap: 'round',
-	eraserWidth: 5,
-	eraserCap: 'round',
-	pencilWidth: 5,
-	pencilCap: 'round',
-	firstColor: '#000',
-	secondColor: '#fff',
-	width: 500,
-	height: 500
+      brushWidth: 5,
+      brushCap: 'round',
+      eraserWidth: 5,
+      eraserCap: 'round',
+      pencilWidth: 5,
+      pencilCap: 'round',
+      firstColor: '#000',
+      secondColor: '#fff',
+      width: 500,
+      height: 500
     };
-
-    var layers = [];
 
     var setProperties = function(updatedProperties) {
-	    updatedProperties = updatedProperties || {};
+      updatedProperties = updatedProperties || {};
 
-	    var prop;
-	    for (prop in updatedProperties) {
-	        if (updatedProperties.hasOwnProperty(prop)) {
-			properties[prop] = updatedProperties.prop;
-		}
-	    }
+      var prop;
+      for (prop in updatedProperties) {
+        if (updatedProperties.hasOwnProperty(prop)) {
+          properties[prop] = updatedProperties.prop;
+        }
+      }
     };
 
-    var getLayers = function() {
-	    return layers;
+    var layers = [],
+        nextLayerId = 0;
+
+    var getNextLayerId = function() {
+      return nextLayerId++;
+    };
+
+    // Layer constructor
+    var Layer = function() {
+      this.id = getNextLayerId();
+      this.globalCompositeOperation = 'source-over';
+      this.globalAlpha = 1.0;
     };
 
     var addLayer = function() {
-    
+      layers.push(new Layer());
     };
 
-    return {};
+    var getLayers = function() {
+      return layers;
+    };
+
+    return {
+      properties: properties,
+      setProperties: setProperties,
+      addLayer: addLayer,
+      getLayers: getLayers
+    };
   });
