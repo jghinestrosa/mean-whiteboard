66d0beefe5aa36f2cd58caf91b9f76fd135b1ef6
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-13 21:51:37 +0100

    Link WhiteboardCtrl scope to canvasFactory
    Link WhiteboardCtrl scope to some canvasFactory properties: 'properties'
    and 'getLayers'. Update whiteboard directive to link 'scope.properties'
    instead 'properties' and 'scope.getLayer'.

--- app/scripts/controllers/whiteboard.js
+++ app/scripts/controllers/whiteboard.js
@@ -2,5 +2,9 @@
 
 angular.module('meanWhiteboardApp')
   .controller('WhiteboardCtrl', function ($scope, canvasFactory) {
+    
+    $scope.properties = canvasFactory.properties;
+    $scope.getLayers = canvasFactory.getLayers;
+    canvasFactory.addLayer();
 
   });
