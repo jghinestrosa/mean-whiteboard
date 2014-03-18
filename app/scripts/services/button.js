82bd3dd4dae88feb70195427e17099a60316f515
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-18 23:07:38 +0100

    Create button factory
    Create button factory with methods to add a new button and get the total
    number of buttons.

--- app/scripts/services/button.js
+++ app/scripts/services/button.js
@@ -2,9 +2,46 @@
 
 angular.module('meanWhiteboardApp')
   .factory('buttonFactory', function () {
-    // Service logic
-    // ...
+
+    /** Private objects and functions **/
+    var buttonsMap = {},
+        nextButtonId = 0,
+        numberOfButtons = 0;
+
+    var getNextButtonId = function() {
+      return nextButtonId++;
+    };
+
+    var Button = function(id, name, caption, img) {
+      this.name = name || ('button' + id);
+      this.caption = caption || ('button' + id);
+      this.img = img || '';
+    };
+
+    /** Public methods **/
+    var addNewButton = function(name, caption, img) {
+      
+      // params
+      var id = getNextButtonId();
+      name = name || ('button' + id);
+      caption = caption || ('button' + id);
+      img = img || '';
+
+      // create a new button and add it to the map
+      var button = new Button(id);
+      buttonsMap[id] = button;
+      numberOfButtons++;
+
+    };
+
+    var getNumberOfButtons = function() {
+      return numberOfButtons;
+    };
 
     // Public API here
-    return {};
+    return {
+      addNewButton: addNewButton,
+      getNumberOfButtons: getNumberOfButtons
+    };
+
   });
