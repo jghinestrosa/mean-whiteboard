ec5d5f8bdd0d92fa6e41307f0595055ae78a2b71
jorge <jorgegarciahinestrosa@gmail.com>
2014-03-18 22:27:33 +0100

    Modify width and height of whiteboard
    Modify width and height of canvas using CSS and replace the width and
    height value of the rest of the componentes of whiteboard to '100%'.
    Whiteboard position set to relative.

--- app/scripts/directives/layerId.js
+++ app/scripts/directives/layerId.js
@@ -6,6 +6,9 @@ angular.module('meanWhiteboardApp')
       restrict: 'A',
       link: function(scope, element, attrs) {
 
+        element[0].width = element[0].offsetWidth;
+        element[0].height = element[0].offsetHeight;
+
         // When layerId is loaded, the model is updated with the context of the canvas
         attrs.$observe('layerId', function(id) {
           scope.setContextToLayer(id, element[0].getContext('2d'));
