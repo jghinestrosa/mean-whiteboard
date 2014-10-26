8bd25063c561522059f072e0526c976e4ee59067
jorge <jorgegarciahinestrosa@gmail.com>
2014-10-26 02:05:54 +0100

    Add whiteboard size from CSS

--- app/scripts/directives/whiteboard.js
+++ app/scripts/directives/whiteboard.js
@@ -6,14 +6,6 @@ angular.module('meanWhiteboardApp')
       templateUrl: 'templates/whiteboard.html',
       restrict: 'E',
       link: function(scope, element, attrs) {
-        var whiteboard = angular.element('#whiteboard');
-        var whiteboardBackground = angular.element('#whiteboard-background');
-        //var windowSize =  {width: $window.innerWidth, height: $window.innerHeight};
-        var windowSize =  {width: '1920px', height: '1080px'};
-
-        whiteboard.css(windowSize);
-        whiteboardBackground.css(windowSize);
-        
       }
     };
   }]);
